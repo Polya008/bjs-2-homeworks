aca6c00a2d602cd632b20882f6fa6b6abe2a4709
Екатерина Воробьёва <polskaya007@mail.ru>
2022-03-13 21:18:35 +0300

    version 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,23 +1,17 @@
 'use strict';
 
 function solveEquation(a,b,c){
-  let x;
   let result = [];
   let discriminant = b ** 2 - 4 * a * c;
   
-  if(discriminant < 0){
-    return result;
-  } else if(discriminant === 0){
-    x = -b / (2*a);
-    result.push(x);
-    return result;
+ if(discriminant < 0){
+    result;
+ } else if(discriminant === 0){
+    return [-b / (2*a)];
   } else {
-    x = (-b + Math.sqrt(discriminant) )/(2*a);
-    result.push(x);
-    x = (-b - Math.sqrt(discriminant) )/(2*a);
-    result.push(x);
-    return result;
-  }
+    return [(-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a)];
+   }
+  return result;
 }
 
 
@@ -28,41 +22,41 @@ function solveEquation(a,b,c){
 function calculateTotalMortgage(percent, contribution, amount, date) {
 let totalAmount;
 
-let percentNumber = Number(percent);
-let contributionNumber = Number(contribution);
-let amountNumber = Number(amount);
+let percentNumber = parseInt(percent);
+let contributionNumber = parseInt(contribution);
+let amountNumber = parseInt(amount);
 
 let creditSum = amountNumber - contributionNumber;
 
-if(typeof percentNumber != 'number'){
-  return (`Параметр ${placeholder} содержит неправильное значение ${percentNumber}`);
-} else if(typeof contributionNumber != 'number'){
-  return (`Параметр ${placeholder} содержит неправильное значение ${contributionNumber}`);
-} else if(typeof amountNumber != 'number'){
-  return (`Параметр ${placeholder} содержит неправильное значение ${amountNumber}`);
+if(Number.isNaN(percentNumber)){
+  return (`Параметр ${percentNumber} содержит неправильное значение ${percentNumber}`);
+} else if(Number.isNaN(contributionNumber)){
+  return (`Параметр ${contributionNumber} содержит неправильное значение ${contributionNumber}`);
+} else if(Number.isNaN(amountNumber)){
+  return (`Параметр ${amountNumber} содержит неправильное значение ${amountNumber}`);
 }
 
 
 //вычисление кол-ва месяцев
-let dayToday =  new Date();
+let dayToday =  Date.now();
 let diffirenceInMonth;
 
 function creditPeriod(a,b) {
-const oneMonth = (1000 * 60 * 60 * 30) / 12;
-a.parse;
-b.parse;
-let diffirence = b.getTime() - a.getTime();
-diffirenceInMonth = Math.round(diffirence / oneMonth);
-return diffirenceInMonth;
+let currentMonth = new Date().getMonth();
+let currentYear = new Date().getFullYear();
+let diffirenceInYears = date.getFullYear - currentYear;
+let diffirenceInMonth = diffirenceInYears * 12 - currentMonth + b.getMonth();
+return  diffirenceInMonth;
 }
 creditPeriod(dayToday, date)
   
 
-let P = (percentNumber / 100) / 12;
+let P = (percentNumber / 100) * 12;
 let totalSum = creditSum * (P + (P / (((1 + P) ^ diffirenceInMonth)- 1 )));
-totalSum.toFixed(2);
 
-totalAmount = Number(totalSum);
+totalSum.toFixed(2);
+totalAmount = parseFloat(totalSum);
+
   console.log(totalAmount);
   return totalAmount;
 }
