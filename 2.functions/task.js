349b6364dc2b2d6a99934322ff525a196164aca8
Екатерина Воробьёва <polskaya007@mail.ru>
2022-03-17 18:59:07 +0300

    1,2 objective

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,27 +1,38 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min = arr[0];
+  let max = min;
+  let sum = 0;
+  
+  for(let i = 0; i < arr.length; i++){
+  if(arr[i] > max){
+    max = arr[i];
+  } else if(arr[i] < min){
+    min = arr[i];
+  } 
+  sum += arr[i];
+  }
+  let avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+    }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
-  
+  for(let i = 0; i < arrOfArr.length; i++){
+  let oneArr = arrOfArr[i];
+  return oneArr;
+  let max = [];
+  max =  func(oneArr);
+    }
   return max;
 }
 
