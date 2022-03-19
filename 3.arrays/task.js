b4578a07aae08a1be79ad699eb901d3c4fc1d2e8
Екатерина Воробьёва <polskaya007@mail.ru>
2022-03-19 20:05:04 +0300

    Done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = (arr1.length == arr2.length) && arr1.every((item, ind) => item === arr2[ind]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((number) => number % 3 === 0 && number > 0).map((number) => number * 10);
   return resultArr; // array
 }
