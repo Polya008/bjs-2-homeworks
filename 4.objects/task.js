9859a229d6da61203bcb92b1255fd01578cca223
Екатерина Воробьёва <polskaya007@mail.ru>
2022-03-31 12:21:47 +0400

    version 3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,8 +4,8 @@ function Student(name, gender, age) {
   this.age = age;
 }
 
-const student1 = new Student('Paul', 'man', 20);
-const student2 = new Student('Alex', 'man', 19);
+const student11 = new Student('Paul', 'man', 20);
+const student22 = new Student('Alex', 'man', 19);
 const student3 = new Student('Kate', 'woman', 21);
 const student4 = new Student('Andrew', 'man', 20);
 
@@ -13,28 +13,28 @@ Student.prototype.setSubject = function(subjectName) {
   this.subjectName = subjectName;
 }
 
-Student.prototype.addMark = function(mark){  
+Student.prototype.addMark = function(mark){ 
     if(this.marks === undefined){ 
-     this.marks.push(mark);
-    } else {
-    this.marks = marks.push(mark);
+     this.marks = [mark];
+    }else{
+   this.marks.push(mark);
     }
 }
 
-Student.prototype.addMarks = function(...mark){  
-    this.marks = marks.push;
+Student.prototype.addMarks = function(...mark){ 
+    this.marks = this.marks.push[mark];
   }  
 
-Student.prototype.getAverage = function(...marks){  
+Student.prototype.getAverage = function(marks){
    let sum = 0;
-   for(let i = 0; i < marks.length; i++){
-    sum +=  marks[i];
+   for(let i = 0; i < this.marks.length; i++){
+    sum += this.marks[i];
    }
-   return  'sum / marks.length';
+   return sum / this.marks.length;
   }
 
 Student.prototype.exclude = function(reason) {
-  delete Student.setSubject;
+  delete Student.subjectName;
   delete Student.marks;
   this.reason = reason;
 }
