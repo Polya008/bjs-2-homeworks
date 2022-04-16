4a58b89ab990f4bb5fc00eaf2463167e043ea78e
Екатерина Воробьёва <polskaya007@mail.ru>
2022-04-16 20:16:47 +0400

    correction 1 task

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,32 +1,33 @@
 class AlarmClock {
 
-    constructor(alarmCollection, timerId) {
+    constructor() {
         this.alarmCollection = [];
-        this.timerId;
+        this.timerId = null;
     }
 
     addClock(time, callback, id) {
         this.time = time;
         this.callback = callback;
-        this.timerId = timerId;
-        if (this.timerId === undefined) {
-            throw new error('Вы забыли передать id');
+        this.id = id;
+        if (!id) {
+            throw new Error('Вы забыли передать id');
         }
 
-        if (this.alarmCollection.some(id)) {
+        if (this.alarmCollection.some((value) => value === id)) {
             console.error('Такой звонок уже существует');
         } else {
             this.alarmCollection.push(id);
         }
+
         this.alarmCollection.push({
-            timerId: timerId,
+            time: time,
             callback: callback,
-            time: time
+            id: id
         });
     }
 
     removeClock(id) {
-        if (this.alarmCollection.findIndex(id)) {
+        if (this.alarmCollection.findIndex((item) => item === id)) {
             this.alarmCollection.splice(item, 1);
             return true;
         } else {
@@ -35,18 +36,35 @@ class AlarmClock {
     }
 
     getCurrentFormattedTime() {
-        return Date.now();
+        return new Date().toLocaleTimeString("ru-Ru", {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
     }
 
-    /*start(){
-      checkClock(id) => this.time === Date.now() {
-        callback();
-      }
 
-    }*/
+
+    start() {
+        function checkClock(ring) {
+            if ((ring) => this.time === this.getCurrentFormattedTime()) {
+                return ring.callback();
+            }
+        }
+        if (!this.id) {
+            const interval = setInterval(() => this.alarmCollection.forEach((ring) => checkClock()));
+        }
+    }
+
+
+
 
     printAlarms() {
         this.alarmCollection.forEach((item) => console.log(item.id, item.time));
     }
 
+
+    clearAlarms(){
+        this.stop();
+        this.alarmCollection.splice(0,this.alarmCollection.length);
+  }
 }
